112be1b1b4d7fe94dc5ba21c5dd4d6f12224f3e4
leonardojsx <leonardosilvajose9984@gmail.com>
2025-10-27 08:43:35 -0300

    fix

--- knexfile.mjs
+++ knexfile.mjs
@@ -3,11 +3,11 @@ import Knex from "knex";
 const knexConfig = {
   client: 'mysql2',
   connection: {
-    host: '127.0.0.1',
-    port: 3306,
-    user: 'root',
-    password: '1234',
-    database: 'treinamentos'
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
   }
 }
 
