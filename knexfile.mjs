c1679b73c6710906091114477b5483cfeabeb588
leonardojsx <leonardosilvajose9984@gmail.com>
2025-11-18 13:32:00 -0300

    fix

--- knexfile.mjs
+++ knexfile.mjs
@@ -8,16 +8,7 @@ const knexConfig = {
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    timezone: 'Z'
-  },
-  pool: {
-    afterCreate: function (conn, done) {
-      // Configurar timezone e modo SQL para compatibilidade
-      conn.query("SET time_zone='+00:00'; SET sql_mode='';", function (err) {
-        done(err, conn);
-      });
-    }
+    database: process.env.DB_NAME
   }
 }
 
